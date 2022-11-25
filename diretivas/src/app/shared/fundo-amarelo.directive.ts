00231a7147ee2a72fbb07f272bcfbbc068c2d37d
Silas Gon <silasidc@gmail.com>
2022-11-24 23:34:32 -0300

    Criando uma diretiva de atributo: ElementRef e Renderer

---
+++ diretivas/src/app/shared/fundo-amarelo.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: 'p[fundoAmarelo]',
+})
+export class FundoAmareloDirective {
+  constructor(private _elementRef: ElementRef, private _renderer2: Renderer2) {
+    //console.log(this._elementRef)
+    //this._elementRef.nativeElement.style.backgroundColor = 'yellow'
+    this._renderer2.setStyle(this._elementRef.nativeElement, 'background-color', 'yellow')
+  }
+}
