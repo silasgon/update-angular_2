164c32ed4bc76f0677f0d719882e846b84e03ea2
Silas Gon <silasidc@gmail.com>
2023-01-22 15:41:10 -0300

    Pipes - pipes parametros e pipes aninhados

---
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent implements OnInit {
+
+  livro: any = {
+    titulo: 'Learning JavaScript Data Structures and Algorithms - Second Edition',
+    rating: 4.9516,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'https://www.amazon.com.br/gp/product/B07P6SZJVQ/ref=dbs_a_def_rwt_hsch_vapi_tkin_p1_i0'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
